6ea90ecdb6685e5b8375bdea3b5d1f87192dde0b
sssadfs <1577707694xy@gmail.com>
2025-08-28 14:57:32 +0800

    重写dataset类来读数据

--- src/dataload.py
+++ src/dataload.py
@@ -1,5 +1,7 @@
+import os
 import torch
-import torchvision
+from PIL import Image
+from torch.utils.data import Dataset
 from torchvision import transforms
 
 BATCH_SIZE = 32
@@ -12,7 +14,35 @@ transformation = transforms.Compose([
                          std=[0.229, 0.224, 0.225]),
 ])
 
-train_dataset = torchvision.datasets.ImageFolder(root = "../tiny-imagenet-200/train",transform=transformation)
+class CustomDataset(Dataset):
+    def __init__(self, root_dir, label_file, transform=None):
+
+        self.root_dir = root_dir
+        self.transform = transform
+        self.samples = []
+
+        with open(label_file, 'r') as f:    # 'r'是读文本模式
+            lines = f.readlines()
+            for line in lines:
+                path, label = line.strip().split(' ')   # 按' '分割
+                full_path = os.path.join(root_dir, path.lstrip("./")) # 拼接路径去掉txt文件中的./
+                self.samples.append((full_path, int(label)))
+
+    def __len__(self):
+        return len(self.samples)
+
+    def __getitem__(self, idx):
+        print(self.samples)
+        img_path, label = self.samples[idx]
+        image = Image.open(img_path).convert('RGB')
+
+        if self.transform:
+            image = self.transform(image)
+
+        return image, label
+
+
+train_dataset = CustomDataset(root_dir="../tiny-imagenet-200/",label_file="../tiny-imagenet-200/train.txt", transform=transformation)
 train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=BATCH_SIZE, shuffle=True)
-val_dataset = torchvision.datasets.ImageFolder(root = "../tiny-imagenet-200/val",transform=transformation)
+val_dataset = CustomDataset(root_dir="../tiny-imagenet-200/",label_file="../tiny-imagenet-200/val.txt",transform=transformation)
 val_loader = torch.utils.data.DataLoader(dataset=val_dataset, batch_size=BATCH_SIZE, shuffle=False)
